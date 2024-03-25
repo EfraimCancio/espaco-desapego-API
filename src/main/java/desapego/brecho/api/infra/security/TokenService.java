4615a21a9b5e2d413da292fe2debd78979602c64
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2024-03-24 23:43:15 -0300

    validating token ande get subject

--- src/main/java/desapego/brecho/api/infra/security/TokenService.java
+++ src/main/java/desapego/brecho/api/infra/security/TokenService.java
@@ -1,6 +1,7 @@
 package desapego.brecho.api.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import desapego.brecho.api.domain.user.User;
@@ -27,6 +28,19 @@ public class TokenService {
                         .sign(algorithm);
             } catch (JWTCreationException exception){
                 throw new RuntimeException("Erro ao gerar token jwt", exception);
+            }
+        }
+
+        public String getSubject(String tokenJWT) {
+            try {
+                var algorithm  = Algorithm.HMAC256(secret);
+                return JWT.require(algorithm)
+                        .withIssuer("API espaco_desapego_brecho.")
+                        .build()
+                        .verify(tokenJWT)
+                        .getSubject();
+            } catch (JWTVerificationException exception){
+                throw new RuntimeException("Token JWT inválido ou expirado");
             }
         }
 
